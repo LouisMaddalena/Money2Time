fe5bf920e990d7892758a6f6927faf112d7d6dc0
LouisMaddalena <62859982+LouisMaddalena@users.noreply.github.com>
2023-03-04 18:44:48 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,30 @@
-console.log("Money2Time");
-
 // Retrieve storage
-chrome.storage.local.get(["hourlyRate"]).then((result) => {
-  console.log(`Hourly rate stored in 'local' is ${result.hourlyRate}`);
+chrome.storage.local.get(["hourlyRate"], function(result) {
+  var optionsUrl = chrome.runtime.getURL("options.html");
 
-  document.querySelectorAll(".a-price").forEach(function (element) {
-    var price = parseFloat(element.innerText.replace("$", ""));
-    element.innerText =
-      "$" +
-      price +
-      "\n" +
-      Math.round((price / result.hourlyRate) * 100) / 100 +
-      " hours of work";
-  });
+  if (isNaN(result.hourlyRate)) {
+    var popup = document.createElement('div');
+    popup.id = "Money2TimePopup";
+    popup.style.position = "fixed";
+    popup.style.top = "10px";
+    popup.style.right = "10px";
+    popup.style.background = "white";
+    popup.style.border = "1px solid black";
+    popup.style.padding = "10px";
+    popup.style.zIndex = "999999";
+    popup.innerHTML = "Money2Time has not been set up, please enter your hourly rate on the <a href='" + optionsUrl + "' target='_blank'>options page</a>";
+    document.body.appendChild(popup);
+  } else {
+    console.log(`Hourly rate stored in 'local' is ${result.hourlyRate}`);
+
+    document.querySelectorAll(".a-price").forEach(function (element) {
+      var price = parseFloat(element.innerText.replace("$", ""));
+      var hours = Math.round((price / result.hourlyRate) * 100) / 100;
+      if (isNaN(hours)) {
+        element.innerText = "$" + price;
+      } else {
+        element.innerText = "$" + price + "\n" + hours + " hours of work";
+      }
+    });
+  }
 });
